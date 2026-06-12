a30f030343837b72d0f0f079963f615e2059ebd6
teo👋 <259869492+teodeloso106@users.noreply.github.com>
2026-06-12 12:58:30 +0800

    ->btapp update : database adjustment for vercel db.js

--- prjbtapp/lib/db.js
+++ prjbtapp/lib/db.js
@@ -5,9 +5,26 @@
  */
 
 import Database from 'better-sqlite3';
+import fs from 'fs';
+import path from 'path';
 
 // Create the database
-const db = new Database('budget.db');
+// const db = new Database('budget.db');
+const templateDbPath = path.join(process.cwd(), 'budget.db'); 
+const writeableDbPath = path.join('/tmp', 'budget.db');
+
+try {
+  if (!fs.existsSync(writeableDbPath)) {
+    if (fs.existsSync(templateDbPath)) {
+      fs.copyFileSync(templateDbPath, writeableDbPath);
+      // at this point, file is successfully copied
+    }
+  }
+} catch (error) {
+  console.error("[ lib/db.js ] Failed to copy database file:", error);
+}
+
+const db = new Database(writeableDbPath);
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS budgetUsers (
